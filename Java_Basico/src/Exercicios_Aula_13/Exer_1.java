7bf88d8da27bfe36ea47fd43a6a68100c9f0cbf2
Wesley A.V <wesley.contato@hotmail.com>
2016-01-29 11:27:12 -0200

    Exer_1

--- Java_Basico/src/Exercicios_Aula_13/Exer_1.java
+++ Java_Basico/src/Exercicios_Aula_13/Exer_1.java
@@ -6,9 +6,10 @@ package Exercicios_Aula_13;
 import java.util.Scanner;
 
 public class Exer_1 {
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Alo mundo");
-        
+
     }
 }
