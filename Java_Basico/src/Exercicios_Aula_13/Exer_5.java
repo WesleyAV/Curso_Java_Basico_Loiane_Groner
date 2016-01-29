f6bed41ec7b61e58c25547629f105a83773d2b06
Wesley A.V <wesley.contato@hotmail.com>
2016-01-29 11:56:17 -0200

    Exer_6

--- Java_Basico/src/Exercicios_Aula_13/Exer_5.java
+++ Java_Basico/src/Exercicios_Aula_13/Exer_5.java
@@ -5,16 +5,16 @@ package Exercicios_Aula_13;
 
 import java.util.Scanner;
 
-
 public class Exer_5 {
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+
         System.out.println("Digite uma medida em metros: (utilize vírgula se for o caso)");
         double m = scan.nextDouble();
-        
+
         double cm = m * 100;
-        
-        System.out.println(+ m + " metros equivale a " + cm + " centímetros. ");
+
+        System.out.println(+m + " metros equivale a " + cm + " centímetros. ");
     }
 }
