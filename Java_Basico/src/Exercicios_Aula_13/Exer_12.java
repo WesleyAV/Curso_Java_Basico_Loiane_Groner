2e84abed6dde99f50808fa31160088b163f81125
Wesley A.V <wesley.contato@hotmail.com>
2016-01-29 13:00:03 -0200

    Exer_12

--- Java_Basico/src/Exercicios_Aula_13/Exer_12.java
+++ Java_Basico/src/Exercicios_Aula_13/Exer_12.java
@@ -1,7 +1,7 @@
 /*
 Tendo como dados de entrada a altura de uma pessoa, construa um
 algoritmo que calcule seu peso ideal, usando a seguinte fórmula:
-(72.7*altura) - 58
+(72.7*altura) - 58.
  */
 package Exercicios_Aula_13;
 
