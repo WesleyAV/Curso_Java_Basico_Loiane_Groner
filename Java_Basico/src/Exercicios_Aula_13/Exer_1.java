42ba6327108f88449e85b87ed1c8b295ed69442c
Wesley A.V <wesley.contato@hotmail.com>
2016-01-29 11:19:10 -0200

    Exercício 1

---
+++ Java_Basico/src/Exercicios_Aula_13/Exer_1.java
@@ -0,0 +1,18 @@
+/*
+Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+ */
+package Exercicios_Aula_13;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author wesley.vieira
+ */
+public class Exer_1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Alo mundo");
+        
+    }
+}
