850214ce365895f0283cf3ad2c25b53285727dfa
Wesley A.V <wesley.contato@hotmail.com>
2016-01-29 11:48:43 -0200

    Exer_5

--- Java_Basico/src/Exercicios_Aula_13/Exer_4.java
+++ Java_Basico/src/Exercicios_Aula_13/Exer_4.java
@@ -5,25 +5,25 @@ package Exercicios_Aula_13;
 
 import java.util.Scanner;
 
-
 public class Exer_4 {
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+
         System.out.println("Digite a nota 1:");
         int n1 = scan.nextInt();
-        
+
         System.out.println("Digite a nota 2:");
         int n2 = scan.nextInt();
-        
+
         System.out.println("Digite a nota 3:");
         int n3 = scan.nextInt();
-        
+
         System.out.println("Digite a nota 4:");
         int n4 = scan.nextInt();
-        
-        int media = (n1+n2+n3+n4)/4;
-        
+
+        int media = (n1 + n2 + n3 + n4) / 4;
+
         System.out.println("A média é: " + media);
     }
 }
